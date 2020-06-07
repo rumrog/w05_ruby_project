01bde804d73cfa2871479e0c697c0d9fc7cc2a90
Rumen Roger <rumenroger@gmail.com>
2020-06-07 09:38:45 +0100

    Added transaction.rb: TransactionClass initialized

---
+++ models/transaction.rb
@@ -0,0 +1,15 @@
+require_relative('../db/sql_runner')
+
+class Transaction
+
+  attr_reader :id
+  attr_accessor :tag_id, :merchant_id, :amount
+  
+  def initialize(options)
+    @id = options['id'].to_i
+    @tag_id = options['tag_id'].to_i
+    @merchant_id = options['merchant_id'].to_i
+    @amount = options['amount'].to_i
+  end
+
+end
