0eda79519ab9d0ed70b58a93b8ba6647c1439fb4
Rumen Roger <rumenroger@gmail.com>
2020-06-07 16:31:04 +0100

    ROUTING - /transactions

---
+++ controllers/transactions_controller.rb
@@ -0,0 +1,9 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/transaction')
+also_reload('../models/*')
+
+get '/transactions' do
+  @transactions = Transaction.all()
+  erb( :'transactions/index' )
+end
