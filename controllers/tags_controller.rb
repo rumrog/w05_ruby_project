fea1ee20313545a751d529ec64d33e56ef644252
Rumen Roger <rumenroger@gmail.com>
2020-06-07 20:29:56 +0100

    /tags/new creates new tag in form

--- controllers/tags_controller.rb
+++ controllers/tags_controller.rb
@@ -7,3 +7,13 @@ get '/tags' do
   @tags = Tag.all()
   erb( :'tags/index' )
 end
+
+get '/tags/new' do
+  erb( :'tags/new' )
+end
+
+post '/tags' do
+  @tag = Tag.new(params)
+  @tag.save()
+  redirect to ('/tags')
+end
