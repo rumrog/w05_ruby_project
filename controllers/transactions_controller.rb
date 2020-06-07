7b10625de051268d5626dbe97e5df12e75e59014
Rumen Roger <rumenroger@gmail.com>
2020-06-07 21:00:54 +0100

    /transactions/new creates new transaction in form

--- controllers/transactions_controller.rb
+++ controllers/transactions_controller.rb
@@ -7,3 +7,15 @@ get '/transactions' do
   @transactions = Transaction.all()
   erb( :'transactions/index' )
 end
+
+get '/transactions/new' do
+  @tags = Tag.all
+  @merchants = Merchant.all
+  erb( :'transactions/new' )
+end
+
+post '/transactions' do
+  transaction = Transaction.new(params)
+  transaction.save
+  redirect to ('/transactions')
+end
